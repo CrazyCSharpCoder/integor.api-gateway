87b1432989cfbaf52e8c6959999088919508e299
Stephen L <logunov.stepan2001@gmail.com>
2023-04-09 20:17:28 +0700

    Add project files.

---
+++ IntegorApiGatewayService/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace IntegorApiGatewayService
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			app.Run(async context => await context.Response.WriteAsync("Hello World"));
+		}
+	}
+}
